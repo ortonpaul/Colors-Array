3b2e4c85740ae6fb4c788130f843c122067c4fc2
Paul Orton <ortonpaul18@gmail.com>
2019-02-28 08:54:22 -0600

    First commit

---
+++ Colors Array/main.swift
@@ -0,0 +1,17 @@
+// Colors Array Challenge
+// Created by Paul Orton
+// For INFOTC 4405 SP 19
+
+import Foundation
+
+var colors: [String] = ["red", "orange", "yellow", "pink"]
+
+print(colors)
+
+colors.remove(at: 3)
+
+print(colors)
+
+colors.append("green")
+
+print(colors)
